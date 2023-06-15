e1da11dcbc1ba6abedb7dbdf0144484d18b70da6
TuttWaweru <tutt.waweru040@gmail.com>
2023-06-15 16:08:03 -0700

    Create task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const int messageLength = sizeof(message) - 1;
+	write(2, message, messageLength);
+	return (1);
+}
