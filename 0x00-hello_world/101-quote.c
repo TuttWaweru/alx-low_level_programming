0c0d562989ed07471878de25574120d2734990c3
TuttWaweru <tutt.waweru040@gmail.com>
2023-06-15 16:26:07 -0700

    Create task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,11 @@
  */
 int main(void)
 {
-	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char message[] = "and that piece of art is useful\" - "
+				"Dora Korpar, 2015-10-19\n";
+
 	const int messageLength = sizeof(message) - 1;
+
 	write(2, message, messageLength);
 	return (1);
 }
