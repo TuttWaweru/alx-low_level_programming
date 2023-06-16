e71eec5f0ca7c0d03e54ef1220dcf118897c3a78
TuttWaweru <tutt.waweru040@gmail.com>
2023-06-16 16:46:14 -0700

    Create if else while task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
- *
  * main - Entry
  * Return: Always 0 (Success)
  *
  */
-int main(void) {
-    char letter;
+int main(void)
+{
+	char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++) {
-        putchar(letter);
-    }
-    putchar('\n');
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
 
-    return 0;
+	return (0);
 }
-
