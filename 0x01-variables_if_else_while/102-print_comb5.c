4284aac1b1d4be8d675493728f1d5e41a51e7bce
TuttWaweru <tutt.waweru040@gmail.com>
2023-06-16 19:47:16 -0700

    Create if else while task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -27,7 +27,7 @@ int main(void)
 						putchar('0' + tens2);
 						putchar('0' + ones2);
 
-						if (num1 != 99 || num2 != 99)
+						if (num1 != 98 && num2 != 99)
 						{
 							putchar(',');
 							putchar(' ');
